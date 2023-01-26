ddce25eea5325ed57035cccea4fa4ed9e018574c
Ar <rejoagung2223@gmail.com>
2023-01-26 19:13:17 +0700

    update 26 01 2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,15 @@
     }
   }
 
+  const clip = document.querySelectorAll('.hover-to-play');
+  for (let i = 0; i <clip.length; i++) {
+    clip[i].addEventListener('mouseenter',function (e) {
+      clip[i].play()
+    })
+    clip[i].addEventListener('mouseout',function (e) {
+      clip[i].pause()
+    })
+  }
   /**
    * Easy event listener function
    */
@@ -240,5 +249,6 @@
     }
 
   });
+
 
 })()
